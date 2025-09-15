31a3075d3d9b7c4c2845749e91dc94d1ef817b3a
SUKRITIKUDUKH <sukritiku@gmail.com>
2025-09-15 22:55:15 +0530

    Data Type

---
+++ src/Lab_012_Data_Type.java
@@ -0,0 +1,13 @@
+public class Lab_012_Data_Type {
+    public static void main(String[] args) {
+        byte b=10;
+        int i=10;
+        short s=10;
+        char c='A';
+        long l=1234567563l;
+        float f= 23.453f;
+        double d =34.56785643;
+
+
+    }
+}
---
+++ src/sep_java/Lab_013_String.java
@@ -0,0 +1,10 @@
+package sep_java;
+
+public class Lab_013_String {
+    public static void main(String[] args) {
+        String s="sukriti";
+        // s,u,k,r,i,t,i  > char= primitive
+
+
+    }
+}
